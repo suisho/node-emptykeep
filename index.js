02f104b018fc5d07d434caecdb6719f00ee40a61
suisho <suisho000000@gmail.com>
2012-12-29 19:07:44 +0900

    indent

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 (function(module){
-"use strict";
-var path = require("path"),
-    clone = require("clone"),
-    glob = require("glob"),
-    fs = require("fs"),
-    touch = require("touch");
-var gitkeep = function(targetDir, options, cb){
-  if(typeof options == "function") cb = options, options = {};
-  if(!options) options = {};
-  var opt = clone(options);
-  
-  opt.targetDir = path.resolve(targetDir);
-  opt.callback = cb;
-  return new GitKeep(opt);
-};
+  "use strict";
+  var path = require("path"),
+      clone = require("clone"),
+      glob = require("glob"),
+      fs = require("fs"),
+      touch = require("touch");
+  var gitkeep = function(targetDir, options, cb){
+    if(typeof options == "function") cb = options, options = {};
+    if(!options) options = {};
+    var opt = clone(options);
+    
+    opt.targetDir = path.resolve(targetDir);
+    opt.callback = cb;
+    return new GitKeep(opt);
+  };
 
-var GitKeep = function(opt){
-  this.cb = opt.cb;
-  opt.keepFileName = opt.keepFileName || ".gitkeep";
-  var err = null;
-  try{
-    this.touchEmptyKeep(opt.targetDir,opt.keepFileName);
-  }catch(e){
-    err = e;
-  }
-  if(typeof opt.callback == "function"){
-    opt.callback(err);
-  }
-};
+  var GitKeep = function(opt){
+    this.cb = opt.cb;
+    opt.keepFileName = opt.keepFileName || ".gitkeep";
+    var err = null;
+    try{
+      this.touchEmptyKeep(opt.targetDir,opt.keepFileName);
+    }catch(e){
+      err = e;
+    }
+    if(typeof opt.callback == "function"){
+      opt.callback(err);
+    }
+  };
 
 
-GitKeep.prototype.touchEmptyKeep =function(targetDir, touchFileName){
-  var find = glob.sync(targetDir + "/**");
-  var emptyDir = [];
-  if(find.length === 0){
-    return;
-  }
-  find.forEach(function(file){
-    var stat = fs.statSync(file);
-    if(!stat.isDirectory()){
+  GitKeep.prototype.touchEmptyKeep =function(targetDir, touchFileName){
+    var find = glob.sync(targetDir + "/**");
+    var emptyDir = [];
+    if(find.length === 0){
       return;
     }
-    var readdir = fs.readdirSync(file);
-    if(readdir.length > 0){
-      return;
-    }
-    var touchFile = path.join(file , touchFileName);
-    touch.touchSync(touchFile);
-  });
-  return emptyDir;
-};
+    find.forEach(function(file){
+      var stat = fs.statSync(file);
+      if(!stat.isDirectory()){
+        return;
+      }
+      var readdir = fs.readdirSync(file);
+      if(readdir.length > 0){
+        return;
+      }
+      var touchFile = path.join(file , touchFileName);
+      touch.touchSync(touchFile);
+    });
+    return emptyDir;
+  };
 
-module.exports = gitkeep;
+  module.exports = gitkeep;
 })(module);
